33e20d7be8a5bb3972f15636da0e7ea053ba688a
Eric Rysavy <erysavy@iastate.edu>
2017-03-23 15:33:14 -0500

    Skeleton classes

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,4 +1,63 @@
+/**
+ * @author Eric Rysavy, Chris Duncan
+ */
+
+import java.util.ArrayList;
 
 public class GraphProcessor {
-
+	
+	/*
+	 * Constructor. graphData holds the absolute path of a file that stores a directed graph. 
+	 * This file will be of the following format: First line indicates number of vertices.
+	 * Each subsequent line lists a directed edge of the graph. The vertices of this graph 
+	 * are represented as strings. This class should create efficient data structures so that 
+	 * the following public methods run efficiently.
+	 */
+	GraphProcessor(String graphData) {
+		
+	}
+	
+	/*
+	 * Returns the out degree of v.
+	 */
+	int outDegree(String v) {
+		return 0;
+	}
+	
+	/*
+	 * Returns true if u and v belong to the same SCC.
+	 */
+	boolean sameComponent(String u, String v) {
+		return false;
+	}
+	
+	/*
+	 * Return all the vertices that belong to the same SCC as v, including v.
+	 */
+	ArrayList<String> componentVertices(String v) {
+		return null;
+	}
+	
+	/*
+	 * Returns the size of the largest component.
+	 */
+	int largestComponent() {
+		return 0;
+	}
+	
+	/*
+	 * Returns the number of strongly connected components.
+	 */
+	int numComponents() {
+		return 0;
+	}
+	
+	/*
+	 * Returns the BFS path from u to v. This method returns an ArrayList of strings that represents 
+	 * the BFS path from u to v.  First vertex in the path must be u and the last vertex must be v. If 
+	 * there is no path from u to v, then this method returns an empty list.
+	 */
+	ArrayList<String> bfsPath(String u, String v) {
+		return null;
+	}
 }
--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -1,4 +1,42 @@
+/**
+ * @author Eric Rysavy, Chris Duncan
+ */
+
+import java.util.ArrayList;
 
 public class WikiCrawler {
-
+	
+	public static final String BASE_URL = "https://en.wikipedia.org";
+	
+	/*
+	 * Constructor with parameters:
+	 * 1. Relative address of the seed url (within Wiki domain).
+	 * 2. Maximum number of pages to be crawled.
+	 * 3. Name of the file the graph will be written to.
+	 */
+	WikiCrawler(String seedUrl, int max, String fileName) {
+		
+	}
+	
+	/*
+	 * Gets a string that represents the contents of a .html file as
+	 * a parameter. Should return an ArrayList of Strings consisting
+	 * of links from the doc. Method must:
+	 * 1. Extract only wiki links, I.e. only links that are of form /wiki/XXXXX.
+	 * 2. Only extract links that appear after the first occurence of the html tag <p> or <P>.
+	 * 3. Should not extract any wiki link that contains the characters "#" or ":".
+	 * 4. The order of the links in the returned ArrayList must be the same order they appear in the doc.
+	 */
+	ArrayList<String> extractLinks(String doc) {
+		return null;
+	}
+	
+	/*
+	 * This method should construct the web graph over following pages: Consider the first
+	 * max many pages that are visited when you do a BFS with seedUrl. Your program should construct
+	 * the web graph only over those pages. and writes the graph to the file fileName.
+	 */
+	void crawl() {
+		
+	}
 }
